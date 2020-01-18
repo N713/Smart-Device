ccad4140c6441064e40676a68a549604801df27e
ely <ely137137@gmail.com>
2020-01-18 05:02:04 +0300

    Иконки, скрытие попапа

--- source/js/main.js
+++ source/js/main.js
@@ -1,15 +1,6 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
-
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
-});
+const body = document.querySelector(`body`);
+const modal = document.querySelector(`.modal`);
+const buttonOrderCall = body.querySelector(`.page-header__button`);
+const modalCloseButton = modal.querySelector(`.modal__close`);
